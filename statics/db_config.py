c94ccee29371ce07cb889ef6e8b6768f0a97e1fa
Vincent Siauw <jamesvincentsiauw@gmail.com>
2020-04-28 15:04:17 +0700

    add dotenv

--- statics/db_config.py
+++ statics/db_config.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = "GB981UA7YT91"
-engine = create_engine('postgres://kmyuvxeo:YdvWUnLQU-pjXW-Bh1HjpBDvby5I17nv@topsy.db.elephantsql.com:5432/kmyuvxeo')
+engine = create_engine(os.getenv("DB_URI"))
 Session = sessionmaker(bind=engine)
 sess = Session()
 
