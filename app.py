357f5f5b1d42cc3f276426b06cc37710f7d2f0b3
Vincent Siauw <jamesvincentsiauw@gmail.com>
2020-05-10 20:21:21 +0700

    edit spaces

--- app.py
+++ app.py
@@ -113,30 +113,32 @@ def ktp_verification():
 
 @app.route('/bookings', methods=['GET','POST'])
 def process_booking():
-	if request.method == 'GET':
-		if request.args.get('user'):
-			return jsonify(get_by_id(request.args.get('user'), "Booking"))
-		else:
-			return jsonify(get("Booking"))
-	elif request.method == 'POST':
-		new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
-                          start_hour=request.form['start_hour'], end_hour=request.form['end_hour'], total_price=request.form['total_price'],
-                          booked_at=datetime.datetime.now(), isFinished=request.form['isFinished'])
-		return jsonify(new_booking.save())
+    if request.method == 'GET':
+        if request.args.get('user'):
+            return jsonify(get_by_id(request.args.get('user'), "Booking"))
+        else:
+            return jsonify(get("Booking"))
+    elif request.method == 'POST':
+        new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
+                              start_hour=request.form['start_hour'], end_hour=request.form['end_hour'],
+                              total_price=request.form['total_price'], booked_at=datetime.datetime.now(),
+                              isFinished=request.form['isFinished'])
+        return jsonify(new_booking.save())
     
 
 @app.route('/venues', methods=['GET','POST'])
 def process_booking():
-	if request.method == 'GET':
-		if request.args.get('id'):
-			return jsonify(get_by_id(request.args.get('id'), "Venue"))
-		else:
-			return jsonify(get("Booking"))
-	elif request.method == 'POST':
-		new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
-                          start_hour=request.form['start_hour'], end_hour=request.form['end_hour'], total_price=request.form['total_price'],
-                          booked_at=datetime.datetime.now(), isFinished=request.form['isFinished'])
-		return jsonify(new_booking.save())
+    if request.method == 'GET':
+        if request.args.get('id'):
+            return jsonify(get_by_id(request.args.get('id'), "Venue"))
+        else:
+            return jsonify(get("Booking"))
+    elif request.method == 'POST':
+        new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
+                              start_hour=request.form['start_hour'], end_hour=request.form['end_hour'],
+                              total_price=request.form['total_price'], booked_at=datetime.datetime.now(),
+                              isFinished=request.form['isFinished'])
+        return jsonify(new_booking.save())
 
 
 @app.route('/covid-19', methods=['GET'])
