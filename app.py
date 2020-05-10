d087221da80f7f0c627a6ed3b4db0857e179b48c
Vincent Siauw <jamesvincentsiauw@gmail.com>
2020-05-10 17:36:06 +0700

    tambah get venue

--- app.py
+++ app.py
@@ -122,8 +122,22 @@ def process_booking():
 		new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
                           start_hour=request.form['start_hour'], end_hour=request.form['end_hour'], total_price=request.form['total_price'],
                           booked_at=datetime.datetime.now(), isFinished=request.form['isFinished'])
-    return jsonify(new_booking.save())
+		return jsonify(new_booking.save())
     
+
+@app.route('/venues', methods=['GET','POST'])
+def process_booking():
+	if request.method == 'GET':
+		if request.args.get('id'):
+			return jsonify(get_by_id(request.args.get('id'), "Venue"))
+		else:
+			return jsonify(get("Booking"))
+	elif request.method == 'POST':
+		new_booking = Booking(id=generateID(30), user_id=request.form['user_id'], venue_id=request.form['venue_id'],
+                          start_hour=request.form['start_hour'], end_hour=request.form['end_hour'], total_price=request.form['total_price'],
+                          booked_at=datetime.datetime.now(), isFinished=request.form['isFinished'])
+		return jsonify(new_booking.save())
+
 
 @app.route('/covid-19', methods=['GET'])
 def covid_data():
